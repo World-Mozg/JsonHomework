c6f783c505820933ee8e98df0aad52cc45c88d57
World-Mozg <73451241+World-Mozg@users.noreply.github.com>
2021-02-16 19:33:07 +0600

    Update Location.cs

--- JsonHomework/Location.cs
+++ JsonHomework/Location.cs
@@ -6,14 +6,14 @@ namespace JsonHomework
 {
     class Location
     {
-        public string name { get; set; }
-        public string country { get; set; }
-        public string region { get; set; }
-        public double lat { get; set; }
-        public double lon { get; set; }
-        public string timeZoneId { get; set; }
-        public DateTime localTime { get; set; }
-        public int localTimeEpoch { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Region { get; set; }
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+        public string TimeZoneId { get; set; }
+        public DateTime LocalTime { get; set; }
+        public int LocalTimeEpoch { get; set; }
         public double UTCOffset { get; set; }
     }
 }
